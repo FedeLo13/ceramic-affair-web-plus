499b6a97bc7d896aef92f45df836461ee08f1b1a
FedeLo <fede.lopezpe@alum.uca.es>
2025-09-29 12:30:33 +0200

    Added some methods

--- ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/repositories/UsuarioRepo.java
+++ ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/repositories/UsuarioRepo.java
@@ -1,5 +1,6 @@
 package es.uca.tfg.ceramic_affair_web.repositories;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -28,4 +29,26 @@ public interface UsuarioRepo extends JpaRepository<Usuario, Long> {
      * @return Un Optional que contiene el usuario si se encuentra, o vacío si no existe.
      */
     Optional<Usuario> findByEmail(String email);
+
+    /**
+     * Busca un usuario por su token de verificación.
+     * 
+     * @param token El token de verificación del usuario a buscar.
+     * @return Un Optional que contiene el usuario si se encuentra, o vacío si no existe.
+     */
+    Optional<Usuario> findByTokenVerificacion(String token);
+
+    /**
+     * Busca un usuario por su token de recuperación.
+     * @param token El token de recuperación del usuario a buscar.
+     * @return Un Optional que contiene el usuario si se encuentra, o vacío si no existe.
+     */
+    Optional<Usuario> findByTokenRecuperacion(String token);
+
+    /**
+     * Elimina los suscriptores no verificados que han expirado.
+     * 
+     * @param fecha la fecha actual para comparar con la fecha de expiración del token.
+     */
+    void deleteByVerificadoFalseAndFechaExpiracionTokenVerificacionBefore(LocalDateTime fecha);
 }
