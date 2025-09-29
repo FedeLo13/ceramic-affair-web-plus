e9443467bdb7be14547b56167963efdd5220c9f2
FedeLo <fede.lopezpe@alum.uca.es>
2025-09-29 12:32:10 +0200

    Added business logic and endpoints for register, login, verification, password change and recovery

---
+++ ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/controllers/common/AuthController.java
@@ -0,0 +1,176 @@
+package es.uca.tfg.ceramic_affair_web.controllers.common;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.uca.tfg.ceramic_affair_web.DTOs.CambioDTO;
+import es.uca.tfg.ceramic_affair_web.DTOs.LoginDTO;
+import es.uca.tfg.ceramic_affair_web.DTOs.OlvidoDTO;
+import es.uca.tfg.ceramic_affair_web.DTOs.RecuperacionDTO;
+import es.uca.tfg.ceramic_affair_web.exceptions.RecaptchaException;
+import es.uca.tfg.ceramic_affair_web.payload.ApiResponseType;
+import es.uca.tfg.ceramic_affair_web.security.Rol;
+import es.uca.tfg.ceramic_affair_web.services.AuthService;
+import es.uca.tfg.ceramic_affair_web.services.RecaptchaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+
+/**
+ * Controlador para la gestión de inicio de sesión.
+ * Este controlador maneja las solicitudes de inicio de sesión y proporciona endpoints para autenticar usuarios.
+ * 
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("/api/public/auth")
+@Tag(name = "Auth", description = "Controlador para la gestión de autenticación")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private RecaptchaService recaptchaService;
+
+    @PostMapping("/login/user")
+    @Operation(summary = "Iniciar sesión como usuario", description = "Permite a un usuario iniciar sesión con su email y contraseña", tags = { "Auth" })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Inicio de sesión exitoso"),
+        @ApiResponse(responseCode = "400", description = "Datos inválidos proporcionados o reCAPTCHA inválido"),
+        @ApiResponse(responseCode = "401", description = "Credenciales inválidas"),
+        @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
+    public ResponseEntity<ApiResponseType<Map<String, String>>> userLogin(@Valid @RequestBody LoginDTO loginDTO) {
+        // Validar el reCAPTCHA
+        boolean isRecaptchaValid = recaptchaService.verifyRecaptcha(loginDTO.getRecaptchaToken());
+        if (!isRecaptchaValid) {
+            throw new RecaptchaException.Invalido();
+        }
+
+        String token = authService.login(loginDTO, Rol.USER);
+        return ResponseEntity.ok(new ApiResponseType<>(true, "Login successful", Map.of("token", token)));
+    }
+
+    @PostMapping("/login/admin")
+    @Operation(summary = "Iniciar sesión como administrador", description = "Permite a un administrador iniciar sesión con su email y contraseña", tags = { "Auth" })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Inicio de sesión exitoso"),
+        @ApiResponse(responseCode = "400", description = "Datos inválidos proporcionados o reCAPTCHA inválido"),
+        @ApiResponse(responseCode = "401", description = "Credenciales inválidas"),
+        @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
+    public ResponseEntity<ApiResponseType<Map<String, String>>> adminLogin(@Valid @RequestBody LoginDTO loginDTO) {
+        // Validar el reCAPTCHA
+        boolean isRecaptchaValid = recaptchaService.verifyRecaptcha(loginDTO.getRecaptchaToken());
+        if (!isRecaptchaValid) {
+            throw new RecaptchaException.Invalido();
+        }
+
+        String token = authService.login(loginDTO, Rol.ADMIN);
+        return ResponseEntity.ok(new ApiResponseType<>(true, "Login successful", Map.of("token", token)));
+    }
+
+    @PostMapping("/registro")
+    @Operation(summary = "Registrar un nuevo usuario", description = "Permite registrar un nuevo usuario con email y contraseña", tags = { "Auth" })
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Usuario registrado exitosamente"),
+        @ApiResponse(responseCode = "400", description = "Datos inválidos proporcionados, reCAPTCHA inválido o verificación pendiente"),
+        @ApiResponse(responseCode = "409", description = "Email ya registrado"),
+        @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
+    public ResponseEntity<ApiResponseType<String>> register(@Valid @RequestBody LoginDTO loginDTO) {
+        // Validar el reCAPTCHA
+        boolean isRecaptchaValid = recaptchaService.verifyRecaptcha(loginDTO.getRecaptchaToken());
+        if (!isRecaptchaValid) {
+            throw new RecaptchaException.Invalido();
+        }
+
+        authService.register(loginDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponseType<>(true, "Verification email has been sent", "Verification email has been sent"));
+    }
+
+    @GetMapping("/verificar")
+    @Operation(summary = "Verificar un usuario", description = "Verifica un usuario utilizando el token de verificación", tags = { "Auth" })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Usuario verificado exitosamente"),
+        @ApiResponse(responseCode = "404", description = "Usuario no encontrado"),
+        @ApiResponse(responseCode = "410", description = "Token de verificación expirado"),
+        @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
+    public ResponseEntity<Void> verifyUser(@RequestParam String token) {
+        authService.verify(token);
+        return redirectToFrontend("success");
+    }
+
+    @PostMapping("/cambio")
+    @Operation(summary = "Cambiar contraseña", description = "Permite a un usuario cambiar su contraseña proporcionando la antigua y la nueva contraseña", tags = { "Auth" })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Contraseña cambiada exitosamente"),
+        @ApiResponse(responseCode = "400", description = "Datos inválidos proporcionados o reCAPTCHA inválido"),
+        @ApiResponse(responseCode = "401", description = "Credenciales inválidas"),
+        @ApiResponse(responseCode = "404", description = "Usuario no encontrado"),
+        @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
+    public ResponseEntity<ApiResponseType<String>> changePassword(@RequestBody CambioDTO dto) {
+        // Validar el reCAPTCHA
+        boolean isRecaptchaValid = recaptchaService.verifyRecaptcha(dto.getToken());
+        if (!isRecaptchaValid) {
+            throw new RecaptchaException.Invalido();
+        }
+
+        authService.cambiarContrasena(dto.getEmail(), dto.getAntiguaPassword(), dto.getNuevaPassword());
+        return ResponseEntity.ok(new ApiResponseType<>(true, "Password changed successfully", "Password changed successfully"));
+    }
+
+    @PostMapping("/olvido")
+    @Operation(summary = "Solicitar recuperación de contraseña", description = "Permite a un usuario solicitar la recuperación de su contraseña", tags = { "Auth" })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Correo de recuperación enviado"),
+        @ApiResponse(responseCode = "400", description = "Datos inválidos proporcionados o reCAPTCHA inválido"),
+        @ApiResponse(responseCode = "404", description = "Usuario no encontrado"),
+        @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
+    public ResponseEntity<ApiResponseType<String>> solicitarRecuperacion(@RequestBody OlvidoDTO dto) {
+        // Validar el reCAPTCHA
+        boolean isRecaptchaValid = recaptchaService.verifyRecaptcha(dto.getRecaptchaToken());
+        if (!isRecaptchaValid) {
+            throw new RecaptchaException.Invalido();
+        }
+
+        authService.solicitarRecuperacion(dto.getEmail());
+        return ResponseEntity.ok(new ApiResponseType<>(true, "Recovery email has been sent", "Recovery email has been sent"));
+    }
+
+    @PostMapping("/reset")
+    @Operation(summary = "Recuperar contraseña", description = "Permite a un usuario recuperar su contraseña utilizando un token de recuperación", tags = { "Auth" })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Contraseña recuperada exitosamente"),
+        @ApiResponse(responseCode = "404", description = "Usuario no encontrado"),
+        @ApiResponse(responseCode = "410", description = "Token de recuperación expirado"),
+        @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
+    public ResponseEntity<ApiResponseType<String>> recuperarContraseña(@RequestBody RecuperacionDTO dto) {
+        authService.resetPassword(dto.getToken(), dto.getNuevaPassword());
+        return ResponseEntity.ok(new ApiResponseType<>(true, "Password has been reset successfully", "Password has been reset successfully"));
+    }
+
+    private ResponseEntity<Void> redirectToFrontend(String status) {
+        // TODO: Cambiar la URL de origen a la de producción cuando esté disponible
+        String frontendUrl = "http://localhost:5173/confirmation?status=" + status;
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header("Location", frontendUrl)
+                .build();
+    }
+}
---
+++ ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/services/AuthService.java
@@ -0,0 +1,238 @@
+package es.uca.tfg.ceramic_affair_web.services;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import es.uca.tfg.ceramic_affair_web.DTOs.LoginDTO;
+import es.uca.tfg.ceramic_affair_web.entities.Usuario;
+import es.uca.tfg.ceramic_affair_web.exceptions.AuthException;
+import es.uca.tfg.ceramic_affair_web.exceptions.EmailException;
+import es.uca.tfg.ceramic_affair_web.repositories.UsuarioRepo;
+import es.uca.tfg.ceramic_affair_web.security.JwtUtils;
+import es.uca.tfg.ceramic_affair_web.security.Rol;
+
+/**
+ * Servicio para la autenticación y gestión de usuarios.
+ * 
+ * @version 1.0
+ */
+@Service
+public class AuthService {
+
+    @Autowired
+    private UsuarioRepo usuarioRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private EmailService emailService;
+
+    /**
+     * Método para verificar las credenciales y el rol de un usuario o administrador.
+     * 
+     * @param loginDTO El DTO que contiene el email y la contraseña del usuario.
+     * @param requiredRole El rol requerido para la autenticación (USER o ADMIN).
+     * @return Un token JWT si las credenciales y el rol son válidos.
+     * @throws AuthException.CredencialesInvalidas si las credenciales son inválidas o el rol no coincide.
+     */
+    public String login(LoginDTO loginDTO, Rol requiredRole) {
+        Usuario usuario = usuarioRepo.findByEmail(loginDTO.getEmail())
+            .orElseThrow(() -> new AuthException.CredencialesInvalidas());
+
+        if (!passwordEncoder.matches(loginDTO.getPassword(), usuario.getPassword()) || !usuario.getRoles().contains(requiredRole)) {
+            throw new AuthException.CredencialesInvalidas();
+        }
+
+        return jwtUtils.generateToken(usuario);
+    }
+
+    /**
+     * Método para registrar un nuevo usuario.
+     * 
+     * @param loginDTO El DTO que contiene el email y la contraseña del nuevo usuario.
+     * @throws AuthException.EmailYaRegistrado si ya existe un usuario con el mismo email.
+     */
+    public void register(LoginDTO loginDTO) {
+        // Comprobar si el email ya está registrado
+        Optional<Usuario> usuarioExistente = usuarioRepo.findByEmail(loginDTO.getEmail());
+
+        // Si esta presente...
+        if (usuarioExistente.isPresent()) {
+            Usuario usuario = usuarioExistente.get();
+
+            // ... y está verificado, lanzar excepción
+            if (usuario.isVerificado()) {
+                throw new AuthException.EmailYaRegistrado();
+            }
+
+            // ... y no está verificado, pero el token de verificación aún no expiró, lanzar excepción
+            if(LocalDateTime.now().isBefore(usuario.getFechaExpiracionTokenVerificacion())) {
+                throw new AuthException.VerificacionPendiente();
+            }
+
+            // Si no se lanzó ninguna excepción, es porque el usuario no está verificado y el token ha expirado
+            // Regenerar el token de verificación
+            usuario.regenerarTokenVerificacion();
+            usuarioRepo.save(usuario);
+            enviarCorreoVerificacion(usuario);
+        } else { // Si no esta presente...
+            // Crear un nuevo usuario con rol USER por defecto
+            Usuario nuevoUsuario = new Usuario(
+                loginDTO.getEmail(),
+                passwordEncoder.encode(loginDTO.getPassword()),
+                Set.of(Rol.USER) // Asignar rol USER por defecto
+            );
+
+            usuarioRepo.save(nuevoUsuario);
+            enviarCorreoVerificacion(nuevoUsuario);
+        }
+    }
+
+    /**
+     * Método para verificar la cuenta de un usuario utilizando un token de verificación.
+     * 
+     * @param token El token de verificación enviado al email del usuario.
+     * @throws AuthException.UsuarioNoEncontrado si no se encuentra un usuario con el token proporcionado.
+     * @throws AuthException.TokenExpirado si el token ha expirado.
+     */
+    public void verify(String token) {
+        // Buscar el usuario por el token de verificación
+        Optional<Usuario> optionalUsuario = usuarioRepo.findByTokenVerificacion(token);
+
+        // Si no se encuentra el usuario, lanzar una excepción
+        if (optionalUsuario.isEmpty()) {
+            throw new AuthException.UsuarioNoEncontrado();
+        }
+
+        // Si el token ha expirado, lanzar una excepción
+        Usuario usuario = optionalUsuario.get();
+        if (LocalDateTime.now().isAfter(usuario.getFechaExpiracionTokenVerificacion())) {
+            throw new AuthException.TokenExpirado();
+        }
+
+        // Verificar el usuario
+        usuario.verificar(); // Establecer verificado a true y limpiar el token
+        usuarioRepo.save(usuario);
+    }
+
+    /**
+     * Método para cambiar la contraseña de un usuario autenticado.
+     * 
+     * @param email El email del usuario que desea cambiar su contraseña.
+     * @param antiguaPassword La contraseña actual del usuario.
+     * @param nuevaPassword La nueva contraseña que el usuario desea establecer.
+     * @throws AuthException.UsuarioNoEncontrado si no se encuentra un usuario con el email proporcionado.
+     * @throws AuthException.CredencialesInvalidas si la contraseña actual es incorrecta.
+     */
+    public void cambiarContrasena(String email, String antiguaPassword, String nuevaPassword) {
+        // Buscar el usuario por email
+        Optional<Usuario> optionalUsuario = usuarioRepo.findByEmail(email);
+
+        // Si no se encuentra el usuario, lanzar una excepción
+        if (optionalUsuario.isEmpty()) {
+            throw new AuthException.UsuarioNoEncontrado();
+        }
+
+        Usuario usuario = optionalUsuario.get();
+
+        // Verificar la contraseña actual
+        if (!passwordEncoder.matches(antiguaPassword, usuario.getPassword())) {
+            throw new AuthException.CredencialesInvalidas();
+        }
+
+        // Actualizar la contraseña
+        usuario.setPassword(passwordEncoder.encode(nuevaPassword));
+        usuarioRepo.save(usuario);
+    }
+
+    /**
+     * Método para solicitar la recuperación de contraseña.
+     * 
+     * @param email El email del usuario que solicita la recuperación.
+     * @throws AuthException.UsuarioNoEncontrado si no se encuentra un usuario con el email proporcionado.
+     */
+    public void solicitarRecuperacion(String email) {
+        // Buscar el usuario por email
+        Optional<Usuario> optionalUsuario = usuarioRepo.findByEmail(email);
+
+        // Si no se encuentra el usuario, lanzar una excepción
+        if (optionalUsuario.isEmpty()) {
+            throw new AuthException.UsuarioNoEncontrado();
+        }
+
+        // Generar un nuevo token de recuperación y establecer la fecha de expiración
+        Usuario usuario = optionalUsuario.get();
+        usuario.generarTokenRecuperacion();
+        usuarioRepo.save(usuario);
+
+        // Enviar el correo de recuperación
+        enviarCorreoRecuperacion(usuario);
+    }
+
+    /**
+     * Método para restablecer la contraseña de un usuario utilizando un token de recuperación.
+     * 
+     * @param token El token de recuperación enviado al email del usuario.
+     * @param nuevaPassword La nueva contraseña que el usuario desea establecer.
+     * @throws AuthException.UsuarioNoEncontrado si no se encuentra un usuario con el token proporcionado.
+     * @throws AuthException.TokenExpirado si el token ha expirado.
+     */
+    public void resetPassword(String token, String nuevaPassword) {
+        // Buscar el usuario por el token de recuperación
+        Optional<Usuario> optionalUsuario = usuarioRepo.findByTokenRecuperacion(token);
+
+        // Si no se encuentra el usuario, lanzar una excepción
+        if (optionalUsuario.isEmpty()) {
+            throw new AuthException.UsuarioNoEncontrado();
+        }
+
+        // Si el token ha expirado, lanzar una excepción
+        Usuario usuario = optionalUsuario.get();
+        if (LocalDateTime.now().isAfter(usuario.getFechaExpiracionTokenRecuperacion())) {
+            throw new AuthException.TokenExpirado();
+        }
+
+        // Actualizar la contraseña y limpiar el token de recuperación
+        usuario.setPassword(passwordEncoder.encode(nuevaPassword));
+        usuario.setTokenRecuperacion(null);
+        usuario.setFechaExpiracionTokenRecuperacion(null);
+        usuarioRepo.save(usuario);
+    }
+
+    private void enviarCorreoRecuperacion(Usuario usuario) {
+        String enlace = "http://localhost:5173/reset-password?token=" + usuario.getTokenRecuperacion();
+        String cuerpo = "<p>Hello,</p>"
+                + "<p>Please reset your password by clicking the link below:</p>"
+                + "<a href=\"" + enlace + "\">Reset Password</a>"
+                + "<p>This link will expire in 1 hour.</p>"
+                + "<p>Thank you!</p>";
+        try {
+            emailService.sendEmail(usuario.getEmail(), "Password Recovery", cuerpo);
+        } catch (Exception e) {
+            throw new EmailException.EnvioFallido(e);
+        }
+    }
+
+    private void enviarCorreoVerificacion(Usuario usuario) {
+        String enlace = "http://localhost:8080/api/public/auth/verificar?token=" + usuario.getTokenVerificacion();
+        String cuerpo = "<p>Hello,</p>"
+                + "<p>Please verify your email by clicking the link below:</p>"
+                + "<a href=\"" + enlace + "\">Verify Email</a>"
+                + "<p>This link will expire in 24 hours.</p>"
+                + "<p>Thank you!</p>";
+        try {
+            emailService.sendEmail(usuario.getEmail(), "Email Verification", cuerpo);
+        } catch (Exception e) {
+            throw new EmailException.EnvioFallido(e);
+        }
+    }
+}
