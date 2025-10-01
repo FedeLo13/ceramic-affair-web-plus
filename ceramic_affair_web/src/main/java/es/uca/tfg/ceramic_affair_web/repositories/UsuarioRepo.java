ac266317f236c347a14c57e9893deacb0cb36457
FedeLo <fede.lopezpe@alum.uca.es>
2025-10-01 14:09:03 +0200

    Added missing annotation

--- ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/repositories/UsuarioRepo.java
+++ ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/repositories/UsuarioRepo.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import es.uca.tfg.ceramic_affair_web.entities.Usuario;
 
@@ -13,6 +14,7 @@ import es.uca.tfg.ceramic_affair_web.entities.Usuario;
  * 
  * @version 1.0
  */
+@Repository
 public interface UsuarioRepo extends JpaRepository<Usuario, Long> {
     /**
      * Verifica si existe un usuario con el correo electrónico dado.
