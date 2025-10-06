ea240945dd4c275c3ebef8e684b8679f82197f0f
FedeLo <fede.lopezpe@alum.uca.es>
2025-10-06 14:08:12 +0200

    Added TipoPago

---
+++ ceramic_affair_web/src/main/java/es/uca/tfg/ceramic_affair_web/entities/TipoPago.java
@@ -0,0 +1,11 @@
+package es.uca.tfg.ceramic_affair_web.entities;
+
+/**
+ * Enum que representa los tipos de pago disponibles.
+ * 
+ * @version 1.0
+ */
+public enum TipoPago {
+    BIZUM,
+    TARJETA
+}
